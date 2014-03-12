4e773739e2df0fdd5ec2a8fb66fc71685202bdd4
yedhukrishnan <yedhu_krishnan@yahoo.com>
2014-03-12 21:21:21 +0530

    Add CheckersBoard class with constructor and print_board method/function

---
+++ CheckersBoard.py
@@ -0,0 +1,16 @@
+class CheckersBoard:
+
+    board = []
+
+    def __init__(self):
+        # Representation of board as a list of lists (2D array type)
+        self.board  = [[' ','b',' ','b',' ','b',' ','b'],
+                       ['b',' ','b',' ','b',' ','b',' '],
+                       [' ','b',' ','b',' ','b',' ','b'],
+                       [' ',' ',' ',' ',' ',' ',' ',' '],
+                       [' ',' ',' ',' ',' ',' ',' ',' '],
+                       ['w',' ','w',' ','w',' ','w',' '],
+                       [' ','w',' ','w',' ','w',' ','w'],
+                       ['w',' ','w',' ','w',' ','w',' ']]
+
+# class CheckersBoard ends

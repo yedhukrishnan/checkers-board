d0ce0b7ec5752c21f019de77441305fe84530603
yedhukrishnan <yedhu_krishnan@yahoo.com>
2014-03-12 22:13:13 +0530

    Separate board and display logic

---
+++ printer.py
@@ -0,0 +1,18 @@
+#Other Methods
+
+# Print the board on terminal
+def print_board(wooden_board):
+    def print_line():
+        print '  ---------------------------------'
+
+    column_list = '    a   b   c   d   e   f   g   h'
+    row_num = 8
+    for row in wooden_board:
+        print_line()
+        print row_num,
+        for col in row:
+            print '|', col,
+        print '|'
+        row_num = row_num - 1
+    print_line()
+    print column_list

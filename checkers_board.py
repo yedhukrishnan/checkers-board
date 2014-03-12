84e9688b1f2a0ffd91756e023c78de139774eec4
yedhukrishnan <yedhu_krishnan@yahoo.com>
2014-03-12 23:33:41 +0530

    Refactor CheckersBoard class  * Initialize an empty wooden board  * Set pieces on board for a new game

--- checkers_board.py
+++ checkers_board.py
@@ -1,16 +1,44 @@
 class CheckersBoard:
     
-    # Member variables
-    board = []
+    # Warriors on the battlefield: our pieces
+    white = 'w'
+    black = 'b'
+
+    wooden_board = []
+    initial_white_positions = ['a1', 'c1', 'e1', 'g1', 'b2', 'd2', 'f2', 'h2', 'a3', 'c3', 'e3', 'g3']
+    initial_black_positions = ['b6', 'd6', 'f6', 'h6', 'a7', 'c7', 'e7', 'g7', 'b8', 'd8', 'f8', 'h8']
 
     def __init__(self):
-        # Representation of board as a list of lists (2D array type)
-        self.board  = [[' ','b',' ','b',' ','b',' ','b'],
-                       ['b',' ','b',' ','b',' ','b',' '],
-                       [' ','b',' ','b',' ','b',' ','b'],
-                       [' ',' ',' ',' ',' ',' ',' ',' '],
-                       [' ',' ',' ',' ',' ',' ',' ',' '],
-                       ['w',' ','w',' ','w',' ','w',' '],
-                       [' ','w',' ','w',' ','w',' ','w'],
-                       ['w',' ','w',' ','w',' ','w',' ']]
+        # Initialize empty wooden board. Yes, we prefer you to play on wooden board! Classic look!! :-)
+        # Representation of our board as a list of lists (2D array type). Still thinking of better a representation.
+        self.wooden_board  = [[' ',' ',' ',' ',' ',' ',' ',' '],
+                              [' ',' ',' ',' ',' ',' ',' ',' '],
+                              [' ',' ',' ',' ',' ',' ',' ',' '],
+                              [' ',' ',' ',' ',' ',' ',' ',' '],
+                              [' ',' ',' ',' ',' ',' ',' ',' '],
+                              [' ',' ',' ',' ',' ',' ',' ',' '],
+                              [' ',' ',' ',' ',' ',' ',' ',' '],
+                              [' ',' ',' ',' ',' ',' ',' ',' ']]
 
+    def set_pieces_on_board_for_a_new_game(self):
+        # We cannot play on an empty wooden board. Let's set the pieces on our board.
+        # We use our initial positions list for arranging black and white pieces.
+        for position in self.initial_white_positions:
+            self.set_piece_on_board(position, self.white)
+
+        for position in self.initial_black_positions:
+            self.set_piece_on_board(position, self.black)
+
+    def set_piece_on_board(self, position, piece):
+        [row, column] = self.position_parser(position);
+        self.wooden_board[row][column] = piece
+
+    def position_parser(self, position):
+        # You prefer a4, b7, e6, ... But being a computer, I prefer row and column.
+        # Parser method for converting human readable position into row and column.
+        # We convert ASCII characters to integer numbers from 0 to 7. 
+        # In our wooden board, row starts from bottom. But in computer, row starts from top!
+        column = ord(position[0]) - ord('a')
+        row = 7 - (ord(position[1]) - ord('1'))
+        print row, column
+        return [row, column]

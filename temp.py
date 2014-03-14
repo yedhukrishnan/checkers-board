0709a34dd533cc3b0f829b41310887d2939dc385
yedhukrishnan <yedhu_krishnan@yahoo.com>
2014-03-14 19:47:41 +0530

    Update temp file for testing

--- temp.py
+++ temp.py
@@ -1,19 +1,21 @@
+# TEMP file: for testing ONLY
+
 from checkers_board import *
 from printer import *
 from assets import *
 from helpers import *
 from movement import *
 
-#pi = { 'black': 'b', 'white': 'w' }
+
+# Print empty wooden board
 b = CheckersBoard(piece)
-#p = Printer(b)
 print_board(b.wooden_board)
 
-# initial_white_row_and_columns = 
-
+# Set and print initial board
 b.set_pieces_on_board_for_a_new_game(initial_white_positions, initial_black_positions)
 print_board(b.wooden_board)
 
+# Check if the board returns the correct piece
 print b.get_piece([7, 0])
 
 m = Movement(b, piece)
@@ -22,9 +24,12 @@ m = Movement(b, piece)
 init_pos = position_parser('c3')
 fin_pos = position_parser('b4')
 
+# Make and check a simple move
 m.make_simple_move(init_pos, fin_pos)
 print_board(b.wooden_board)
 
+
+# Make and check a capturing nove
 b.set_piece([4, 5], 'b')
 print_board(b.wooden_board)
 m.make_capture_move([5, 4], [3, 6])
